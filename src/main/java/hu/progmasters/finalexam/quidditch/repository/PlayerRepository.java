af1e3e104d47a85a973168e09b9994df8cd6a437
Gőbl Sámuel Tamás (Samy) <goblsamy@gmail.com>
2023-11-25 15:31:47 +0100

    Query elkezdése, végső ellenőrzés, befejezés.

--- src/main/java/hu/progmasters/finalexam/quidditch/repository/PlayerRepository.java
+++ src/main/java/hu/progmasters/finalexam/quidditch/repository/PlayerRepository.java
@@ -12,4 +12,6 @@ public interface PlayerRepository extends JpaRepository<Player, Long> {
     @Query("select case when (count (p) >:maxValue ) then false else true end from Player p" +
             " where p.club.id=:clubId and p.playerType=:playerType")
     boolean isThereFreeSpaceInTeam(PlayerType playerType, Integer clubId, Integer maxValue);
+
+    @Query("select p.joined, p.wins from Player p order by p. desc and order by p")
 }
